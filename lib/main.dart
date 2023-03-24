bc43b7545e08015b17820c71fcbe68cf95bdd7c2
Firelawless <wahyuvalentino@student.ub.ac.id>
2023-03-25 05:17:41 +0700

    timer pomodoro

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
        
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
       home: Home(),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,15 @@
+import 'dart:async';
+
 import 'package:dot_navigation_bar/dot_navigation_bar.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
 class Home extends StatefulWidget {
   final List<Icon> timesCompleted = [];
-  Home(){
-    
-  }
+  Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
@@ -15,39 +17,175 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int _currentIndex = 0;
+  double defaultValue = 300;
+  double value = 300.0;
+  bool isStarted = false;
+  int focusedMins = 0;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Color(0xFFA9CBE3)));
+    
+  }
+
+  void startTimer() {
+    focusedMins = value.toInt();
+    const oneSec = Duration(seconds: 1);
+    _timer = Timer.periodic(
+      oneSec,
+      (Timer timer) {
+        if (value <= 1) {
+          setState(() {
+            timer.cancel();
+            value = defaultValue;
+            isStarted = false;
+          });
+        } else {
+          setState(() {
+            value--;
+          });
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF5CDBBD),
+      backgroundColor: Color(0xFFA9CBE3),
       body: SafeArea(
-        child: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
           child: Column(
-            children: [
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Pomodoro number: 2',
-                style: TextStyle(fontSize: 32, color: Colors.white),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Set: 3',
-                style: TextStyle(fontSize: 22, color: Colors.white),
-              ),
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              
               Expanded(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [],
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Center(
+                      child: SizedBox(
+                        width: 250,
+                        height: 250,
+                        child: Stack(
+                          children: [
+                            SleekCircularSlider(
+                              initialValue: value,
+                              min: 0,
+                              max: 5401,
+                              appearance: CircularSliderAppearance(
+                                customWidths: CustomSliderWidths(
+                                  trackWidth: 15,
+                                  handlerSize: 10,
+                                  progressBarWidth: 15,
+                                  shadowWidth: 0,
+                                ),
+                                customColors: CustomSliderColors(
+                                  trackColor: Color(0xFF5A7B91),
+                                  progressBarColor: Color(0xFF294A5E),
+                                  hideShadow: true,
+                                  dotColor: Colors.white,
+                                  
+                                ),
+                                size: 250,
+                                angleRange: 360,
+                                startAngle: 270,
+                              ),
+                              onChange: (newValue) {
+                                setState(() {
+                                  value = newValue;
+                                });
+                              },
+                              innerWidget: (double newValue) {
+                                return Center(
+                                  child: Text(
+                                    "${(value ~/ 60).toInt().toString().padLeft(2, '0')}:${(value % 60).toInt().toString().padLeft(2, '0')}",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 46,
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                            if (isStarted)
+                              GestureDetector(
+                                onTap: () {},
+                                child: Container(
+                                  width: 250,
+                                  height: 250,
+                                  color: Colors.transparent,
+                                ),
+                              )
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 100,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (!isStarted) {
+                            isStarted = true;
+                            startTimer();
+                          } else {
+                            _timer.cancel();
+                            value = defaultValue;
+                            isStarted = false;
+                          }
+                        });
+                      },
+                      child: Container(
+                        width: 200,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF5A7B91),
+                          borderRadius: BorderRadius.circular(7),
+                          boxShadow: [
+                            BoxShadow(
+                              spreadRadius: 3,
+                              blurRadius: 3,
+                              offset: const Offset(0, 3),
+                              color: Colors.black.withOpacity(0.1),
+                            )
+                          ],
+                        ),
+                        alignment: Alignment.center,
+                        child: Text(
+                          isStarted ? "STOP" : "START",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
         ),
       ),
+      
       bottomNavigationBar: Padding(
         padding: EdgeInsets.only(bottom: 10),
         child: DotNavigationBar(
